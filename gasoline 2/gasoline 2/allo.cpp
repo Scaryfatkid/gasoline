2971753a411f39abeecdd33d4f4d398a0107b59b
game110pm <game110pm@605-110-STU06>
2017-01-30 13:22:47 -0700

    Aloe

---
+++ gasoline 2/gasoline 2/allo.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+	double gasprice;
+	double tank;
+	double cost;
+	cout << "how much is gas?" << endl;
+	cin >> gasprice;
+	cout << "how big is your tank ;)" << endl;
+	cin >> tank;
+	cost = tank*gasprice;
+	cout << "Your cost will be" << cost << endl;
+
+
+
+
+
+}
